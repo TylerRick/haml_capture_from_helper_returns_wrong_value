855cb7ad286e58e15df3f5d58fea443033b62870
Tyler Rick <github.com@tylerrick.com>
2010-04-16 13:51:20 -0700

    Improved the debug output that gets logged

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,23 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def render_with_contents(*args)
+    Rails.logger.info "!!! Haml::Template::options[:ugly]=#{Haml::Template::options[:ugly].inspect}"
+
     options = args.extract_options!.symbolize_keys
     options[:locals] ||= {}
-    #require 'ruby-debug'; debugger
 
+    #require 'ruby-debug'; debugger
     # This returns "This is the block passed to header\n" -- as expected
     options[:locals][:contents] = capture_haml do
       yield
     end
+    Rails.logger.info "!!! capture returned:      #{options[:locals][:contents].inspect}"
 
     # This returns 2 -- not what one would expect!
     options[:locals][:contents] = capture do
       yield
     end
-
-    Rails.logger.debug "!!! options[:locals][:contents]=#{options[:locals][:contents].inspect}"
+    Rails.logger.info "!!! capture_haml returned: #{options[:locals][:contents].inspect}"
 
     render *args, options
   end
